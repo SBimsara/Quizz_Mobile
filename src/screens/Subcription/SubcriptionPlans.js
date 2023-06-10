e55c70e8120e10bc3b44bae3ced410739158fbfb
Mad Man <113254019+SBimsara@users.noreply.github.com>
2023-06-10 14:02:53 +0530

    added introduction text to subscription plan

--- src/screens/Subcription/SubcriptionPlans.js
+++ src/screens/Subcription/SubcriptionPlans.js
@@ -7,7 +7,15 @@ import { NativeBaseProvider, Input, Button, VStack, Select } from "native-base";
 const SubcriptionPlans = () => {
   return (
     <NativeBaseProvider>
-      <SafeAreaView style={styles.container}></SafeAreaView>
+      <SafeAreaView style={styles.container}>
+        <Text style={[styles.title, styles.moveUp]}>
+          Now let's select a subscription plan
+        </Text>
+        <Text style={[styles.helpingText, styles.moveUp]}>
+          If yoy want more time to decide select the free plan and change it
+          later
+        </Text>
+      </SafeAreaView>
     </NativeBaseProvider>
   );
 };
@@ -18,6 +26,19 @@ const styles = StyleSheet.create({
     backgroundColor: "#F1FBFF",
     alignItems: "center",
     justifyContent: "center",
+  },
+  title: {
+    fontSize: 17,
+    fontWeight: "bold",
+  },
+  helpingText: {
+    textAlign: "center",
+    marginTop: 15,
+    marginRight: 35,
+    marginLeft: 35,
+  },
+  moveUp: {
+    transform: [{ translateY: -240 }],
   },
 });
 export default SubcriptionPlans;
